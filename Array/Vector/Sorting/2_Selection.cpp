507690962e817d8501f189fc554c4de89b8151b7
Priyan90 <priyanshuj662@gmail.com>
2026-02-19 21:13:23 +0530

    function

--- Array/Vector/Sorting/2_Selection.cpp
+++ Array/Vector/Sorting/2_Selection.cpp
@@ -1,24 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for (int i=0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    //Selection sort
-    for ( int i=0; i<n; i++ ){
-        int min=i;
-        for (int j=i+1; j<n; j++){
-            if (arr[j]<arr[min]){
-                min=j;
+    // Selection sort
+    for (int i = 0; i < n; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
             }
-        swap(arr[i],arr[min]);
+            swap(arr[i], arr[min]);
         }
     }
-    cout<<"Sorted array through selection sort"<<endl;
-    for (int i=0; i<n; i++){
-    cout<<arr[i]<<" ";
-}return 0;
+    cout << "Sorted array through selection sort" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
