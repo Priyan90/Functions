973fe64d35c52f54c1c46109e4c2dca54efb8359
Priyan90 <priyanshuj662@gmail.com>
2026-03-02 23:25:35 +0530

    function

---
+++ Array/2d_Araay.cpp/2d_sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[3][3] = {
+        {10, 20, 30},
+        {40, 50, 60},
+        {70, 80, 90}
+    };
+
+    for(int i = 0; i < 3; i++) {          // rows
+        for(int j = 0; j < 3; j++) {      // columns
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
